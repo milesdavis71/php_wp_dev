f7b2a0bb34e74b3bc07b6e703f8b8dfd2773ff83
Hevesi Istvan <hevesiistvan71@hotmail.com>
2022-09-15 14:49:14 +0200

    4. Convert Static HTML Template into WordPress - folyamatban.

--- app/public/wp-content/themes/gyak-theme/functions.php
+++ app/public/wp-content/themes/gyak-theme/functions.php
@@ -1,5 +1,6 @@
 <?php
 function gyak_files(){
+    wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('university_main_styles', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'gyak_files');
